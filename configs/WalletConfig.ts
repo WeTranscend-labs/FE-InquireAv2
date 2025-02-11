f712a4f35c3abeec28aa3e46bff0afb55c88422b
Quantaphocpython <quan02042004@gmail.com>
2025-02-11 16:48:46 +0700

    :sparkles:[feat]: update toggle theme

--- configs/WalletConfig.ts
+++ configs/WalletConfig.ts
@@ -1,6 +1,6 @@
 import { getDefaultConfig, getDefaultWallets } from '@rainbow-me/rainbowkit';
 import { ledgerWallet, trustWallet } from '@rainbow-me/rainbowkit/wallets';
-import { ancient8Sepolia as network } from 'wagmi/chains';
+import { kairos as network } from 'wagmi/chains';
 import { http } from 'wagmi';
 
 const { wallets } = getDefaultWallets();
