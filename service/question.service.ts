c1c5ccc900896f0f2e56340b23f1d1a6b4f5cd0f
Quantaphocpython <quan02042004@gmail.com>
2025-03-07 17:56:09 +0700

    :sparkles:[feat]: create service funtion to interact with answer api

--- service/question.service.ts
+++ service/question.service.ts
@@ -3,7 +3,7 @@ import { CreateQuestionData } from './dto/question/question.create';
 import { ApiResponse } from './dto/apiReponse';
 import { QuestionResponse } from './dto/question/question.response';
 
-const BASE_URL = 'http://localhost:8080/api';
+export const BASE_URL = 'http://localhost:8080/api';
 
 export async function createQuestion(
   data: CreateQuestionData
